7e30d106843f7f4ca2e1d68d5ab8111a58c69bc9
Rajas1312 <itsmerajas2@gmail.com>
2020-11-04 16:48:23 +0530

    dictionary of singlet and percentage

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,9 +1,28 @@
 #!/bin/bash -x
 echo "program on coin flip combination"
-
+a=0
+b=0
 if [ $((RANDOM%2)) -eq 0 ]
 then
 	echo "heads"
 else
 	echo "Tails"
 fi
+
+declare -A combo
+for i in {1..10}
+do
+	if [ $((RANDOM%2)) -eq 0 ]
+	then
+		combo[$i]="H"
+		((a++))
+	else
+		combo[$i]="T"
+		((b++))
+	fi
+	echo " the singlet dictionary for 10 coin flips is" ${combo[@]}
+done
+c=$((a/10*100))
+echo "the percentge of heads is $c"
+d=$((b/10*100))
+echo "the percentage of tails is $d"
